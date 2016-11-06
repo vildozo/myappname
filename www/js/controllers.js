259eb383847602c838d6e91c45d996778770de77
vm mac <vmmac@vms-Mac.local>
2016-11-06 23:18:40 +0800

    NO FUNCIONA LAS DIRECCIONES

---
+++ www/js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('starter.controllers', [])
+.controller('LoginCtrl', function($scope, $state) {
+    $scope.doLogin = function(){
+        //Normally hand off here to a LoginService of some kind and
+        //manage success and failure
+        $state.go('home');
+    }
+});
